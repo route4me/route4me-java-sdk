a5fa18afcd3d7b36d9efd30921b8720ad8a4cc6e
juanroute4me <juan@route4me.com>
2020-06-19 16:32:24 -0500

    Revert "Added missing field on Address Model"
    This reverts commit efe8dbe8bc5d118f22cee53c620bdb8209552176.

--- src/main/java/com/route4me/sdk/services/routing/Address.java
+++ src/main/java/com/route4me/sdk/services/routing/Address.java
@@ -50,10 +50,6 @@ public class Address {
     private Long timeWindowStart;
     @SerializedName("time_window_end")
     private Long timeWindowEnd;
-    @SerializedName("time_window_start_2")
-    private Long timeWindowStart2;
-    @SerializedName("time_window_end_2")
-    private Long timeWindowEnd2;
     @SerializedName("time")
     private Long time;
     @SerializedName("notes")
@@ -64,102 +60,6 @@ public class Address {
     private Map<String, Object>  manifest;
     @SerializedName("order_id")
     private Integer orderId;
-    @SerializedName("group")
-    private String group;
-    @SerializedName("customer_po")
-    private String customerPO;
-    @SerializedName("invoice_no")
-    private String invoiceNumber;
-    @SerializedName("reference_no")
-    private String referenceNumber;
-    @SerializedName("order_no")
-    private String orderNumber;
-    @SerializedName("first_name")
-    private String firstName;
-    @SerializedName("last_name")
-    private String lastName;
-    @SerializedName("email")
-    private String email;
-    @SerializedName("phone")
-    private String phone;
-    @SerializedName("timeframe_violation_state")
-    private Integer timeframeViolationState;
-    @SerializedName("timeframe_violation_time")
-    private Integer timeframeViolationTime;
-    @SerializedName("timeframe_violation_rate")
-    private Double timeframeViolationRate;
-    @SerializedName("curbside_lat")
-    private Double curbsideLatitude;
-    @SerializedName("curbside_lng")
-    private Double curbsideLongitude;
-    @SerializedName("priority")
-    private Integer priority;
-    @SerializedName("geocodings")
-    private List<Geocodings> geocodings;
-    @SerializedName("address_stop_type")
-    private String addressStopType;
-    @SerializedName("timestamp_last_visited")
-    private Long timestampLastVisited;
-    @SerializedName("visited_lat")
-    private Double visitedLatitude;
-    @SerializedName("visited_lng")
-    private Double visitedLongitude;
-    @SerializedName("is_departed")
-    private Boolean isDeparted;
-    @SerializedName("departed_lat")
-    private Double departedLatitude;
-    @SerializedName("departed_lng")
-    private Double departedLongitude;
-    @SerializedName("timestamp_last_departed")
-    private Long timestampLastDeparted;
-    @SerializedName("weight")
-    private Double weight;
-    @SerializedName("cost")
-    private Double cost;
-    @SerializedName("revenue")
-    private Double revenue;
-    @SerializedName("cube")
-    private Double cube;
-    @SerializedName("pieces")
-    private Integer pieces;
-    @SerializedName("drive_time_to_next_destination")
-    private Long driveTimetoNextDestination;
-    @SerializedName("abnormal_traffic_time_to_next_destination")
-    private Long abnormalTrafficTimetoNextDestination;
-    @SerializedName("uncongested_time_to_next_destination")
-    private Long uncongestedTimetoNextDestination;
-    @SerializedName("traffic_time_to_next_destination")
-    private Long trafficTimetoNextDestination;
-    @SerializedName("distance_to_next_destination")
-    private Double distancetoNextDestination;
-    @SerializedName("udu_distance_to_next_destination")
-    private Double uduDistancetoNextDestination;
-    @SerializedName("generated_time_window_start")
-    private Long generatedTimeWindowStart;
-    @SerializedName("generated_time_window_end")
-    private Long generatedTimeWindowEnd;
-    @SerializedName("geofence_detected_visited_timestamp")
-    private Long geofenceDetectedVisitedTimestamp;
-    @SerializedName("geofence_detected_departed_timestamp")
-    private Long geofenceDetectedDepartedTimestamp;
-    @SerializedName("geofence_detected_service_time")
-    private Long geofenceDetectedServiceTime;
-    @SerializedName("geofence_detected_visited_lat")
-    private Double geofenceDetectedVisitedLatitude;
-    @SerializedName("geofence_detected_visited_lng")
-    private Double geofenceDetectedVisitedLongitude;
-    @SerializedName("geofence_detected_departed_lat")
-    private Double geofenceDetectedDepartedLatitude;
-    @SerializedName("geofence_detected_departed_lng")
-    private Double geofenceDetectedDepartedLongitude;
-    @SerializedName("tracking_number")
-    private String trackingNumber;
-    @SerializedName("path_to_next")
-    private List<GeoCoordinates> pathtoNext;
-    @SerializedName("wait_time_to_next_destination")
-    private Long waitTimetoNextDestination;
-
-    
     
     public Address() {
     }
