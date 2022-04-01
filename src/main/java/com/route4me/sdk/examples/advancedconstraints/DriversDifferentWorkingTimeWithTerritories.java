ca9c958f9b5773c16db48da3aa529e850cdfcd06
juanroute4me <juan@route4me.com>
2022-04-01 15:48:22 -0500

    Added Advanced Contraint Example Driver Different Working Time and Territories

--- src/main/java/com/route4me/sdk/examples/advancedconstraints/DriversDifferentWorkingTimeWithTerritories.java
+++ src/main/java/com/route4me/sdk/examples/advancedconstraints/DriversDifferentWorkingTimeWithTerritories.java
@@ -10,10 +10,7 @@ import com.opencsv.CSVReaderBuilder;
 import com.route4me.sdk.exception.APIException;
 import com.route4me.sdk.services.routing.Address;
 import com.route4me.sdk.services.routing.Constants.AlgorithmType;
-import com.route4me.sdk.services.routing.Constants.DeviceType;
-import com.route4me.sdk.services.routing.Constants.DistanceUnit;
 import com.route4me.sdk.services.routing.Constants.OptimizationState;
-import com.route4me.sdk.services.routing.Constants.Optimize;
 import com.route4me.sdk.services.routing.Constants.TravelMode;
 import com.route4me.sdk.services.routing.DataObject;
 import com.route4me.sdk.services.routing.OptimizationParameters;
@@ -32,9 +29,9 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DriversSchedulesWithTerritoriesAndRetailLocation {
+public class DriversDifferentWorkingTimeWithTerritories {
 
-    public static List<Address> readAddressesFromCSV(String filename, List<String> zone1, List<String> zone2, List<String> zone3, Integer serviceTime) {
+    public static List<Address> readAddressesFromCSV(String filename, List<String> territory1, List<String> territory2, List<String> territory3, Integer serviceTime) {
         List<Address> addresses = new ArrayList<>();
         CSVReader csvReader;
         try {
@@ -50,22 +47,22 @@ public class DriversSchedulesWithTerritoriesAndRetailLocation {
 
                 switch (group) {
                     case 0:
-                        address.setTags(zone1);
+                        address.setTags(territory1);
                         break;
                     case 1:
-                        address.setTags(zone2);
+                        address.setTags(territory2);
                         break;
                     case 2:
-                        address.setTags(zone3);
+                        address.setTags(territory3);
                         break;
                 }
                 addresses.add(address);
             }
 
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(DriversSchedulesWithTerritoriesAndRetailLocation.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DriversDifferentWorkingTimeWithTerritories.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(DriversSchedulesWithTerritoriesAndRetailLocation.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DriversDifferentWorkingTimeWithTerritories.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return addresses;
@@ -73,14 +70,15 @@ public class DriversSchedulesWithTerritoriesAndRetailLocation {
     }
 
     //**********************************************************************
-    // TEST CASE: Drivers Schedules with Territories and Retail Location
+    // TEST CASE: Driver's Different Working Time
+    // Drivers have 2 Working Times:
+    // Full Time 8h
+    // Partial Time 4h
     // 2000 Stops
-    // 30 Schedules
     // 3 Territories
-    // Retail Location
     //**********************************************************************
     public static void main(String[] args) {
-        String apiKey = "11111111111111111111111111111111";
+        String apiKey = "EB6B3F9EC63324BEB0A1432697310BD4";
         RoutingManager manager = new RoutingManager(apiKey, Boolean.TRUE);
         OptimizationParameters optParameters = new OptimizationParameters();
 
@@ -91,55 +89,60 @@ public class DriversSchedulesWithTerritoriesAndRetailLocation {
         parameters.setShareRoute(Boolean.FALSE);
         parameters.setRt(Boolean.TRUE);
         parameters.setParts(30);
-        parameters.setRouteName("Drivers Schedules - 3 Territories");
-        parameters.setOptimize(Optimize.DISTANCE.toString());
-        parameters.setDistanceUnit(DistanceUnit.MI.toString());
-        parameters.setDeviceType(DeviceType.WEB.toString());
+        parameters.setRouteName("Drivers Different Working Time Example - Territories");
         parameters.setTravelMode(TravelMode.DRIVING.toString());
 
         // **********************
         // Territories
         // **********************
-        List<String> zone1 = Arrays.asList("ZONE 01");
-        List<String> zone2 = Arrays.asList("ZONE 02");
-        List<String> zone3 = Arrays.asList("ZONE 03");
+        List<String> territory1 = Arrays.asList("ZONE 01");
+        List<String> territory2 = Arrays.asList("ZONE 02");
+        List<String> territory3 = Arrays.asList("ZONE 03");
 
         // **********************
         // Schedules
         // **********************
         AdvancedConstraints schedule;
-        List<String> zone = new ArrayList<>();
+        List<String> territory = new ArrayList<>();
         List<AdvancedConstraints> advancedConstraints = new ArrayList<>();
 
-        Address retailLocationAddress = new Address("RETAIL LOCATION", "RETAIL LOCATION",  25.8741751,-80.1288583, 300);
-         
-        List <Object> locationsSequence = Arrays.asList("", retailLocationAddress);
+        Integer memberCount = 0;
+        Integer timeWindowEnd = 0;
         
-        
-        
-        for (int i = 0; i < 30; i++) {
-            schedule = new AdvancedConstraints();
-            switch (i % 3) {
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
                 case 0:
-                    zone = zone1;
+                    territory = territory1;
                     break;
                 case 1:
-                    zone = zone2;
+                    territory = territory2;
                     break;
                 case 2:
-                    zone = zone3;
+                    territory = territory3;
                     break;
             }
 
-            schedule.setTags(zone);
-            schedule.setMembersCount(1);
-            List<List<Integer>> timeWindowsSchedule = new ArrayList<>();
-            List<Integer> timeWindowSchedule = Arrays.asList((8 + 5) * 3600, (11 + 5) * 3600);
-            timeWindowsSchedule.add(timeWindowSchedule);
-            schedule.setAvailableTimeWindows(timeWindowsSchedule);
-            schedule.setLocationSequencePattern(locationsSequence);
+            for (int j = 0; j < 2; j++) {
+                switch (j) {
+                    case 0:
+                        memberCount = 10;
+                        timeWindowEnd = (11 + 5) * 3600; //Part Time
+                        break;
+                    case 1:
+                        memberCount = 20;
+                        timeWindowEnd = (15 + 5) * 3600; //Full Time
+                        break;
+                }
+                schedule = new AdvancedConstraints();
+                schedule.setTags(territory);
+                schedule.setMembersCount(memberCount);
+                List<List<Integer>> timeWindowsSchedule = new ArrayList<>();
+                List<Integer> timeWindowSchedule = Arrays.asList((7 + 5) * 3600, timeWindowEnd);
+                timeWindowsSchedule.add(timeWindowSchedule);
+                schedule.setAvailableTimeWindows(timeWindowsSchedule);
 
-            advancedConstraints.add(schedule);
+                advancedConstraints.add(schedule);
+            }
         }
 
         // Schedules registration
@@ -156,7 +159,7 @@ public class DriversSchedulesWithTerritoriesAndRetailLocation {
         Path filePath = Paths.get(currentPath.toString(),"src", "main", "java", "com", "route4me", "sdk", "examples", "advancedconstraints", "locations.csv");
 
         
-        addresses = readAddressesFromCSV(filePath.toString(), zone1, zone2, zone3, serviceTime);
+        addresses = readAddressesFromCSV(filePath.toString(), territory1, territory2, territory3, serviceTime);
         
         Address address = new Address("DEPOT", true, 25.723025, -80.452883, 0);
         addresses.add(address);
